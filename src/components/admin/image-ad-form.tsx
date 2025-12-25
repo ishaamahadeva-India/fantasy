76096e3f3e833ac905bf56ef9bff36b8b41414e0
download--17- <kranthisuryadevara25@gmail.com>
2025-12-25 19:20:52 +0530

    Add missing Link import in image-ad-form

--- src/components/admin/image-ad-form.tsx
+++ src/components/admin/image-ad-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { useFirestore, useUser } from '@/firebase';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
